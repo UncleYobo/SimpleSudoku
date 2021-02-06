ebfbb4bd22dbf9a09cb688db59d6c67bc61236b5
UncleYobo <jhsproyer@gmail.com>
2021-02-06 12:24:43 -1000

    System strip

--- Assets/Scripts/GameplayManager.cs
+++ Assets/Scripts/GameplayManager.cs
@@ -5,111 +5,12 @@ using UnityEngine.SceneManagement;
 
 public class GameplayManager : MonoBehaviour
 {
-    public GameObject CurrentSelection;
-    public Color SelectionColor;
-
-    public Generator.GameMode Difficulty;
-    public Generator _boardGenerator;
-    public CheckGroup[] checkGroups;
-
     public bool Ads;
-
-    private GameObject _previousSelection;
-    private bool _isDirty;
     private AdHandler _ads;
-
-    private void OnEnable()
-    {
-        SceneManager.sceneLoaded += SceneChanged;
-    }
-    private void OnDisable()
-    {
-        SceneManager.sceneLoaded -= SceneChanged;
-    }
-
-    void SceneChanged(Scene scene, LoadSceneMode mode)
-    {
-        
-    }
-
-    void Start()
-    {
-        DontDestroyOnLoad(this);
-        _ads = GetComponent<AdHandler>();
-    }
 
     public void OnNewGameStarted()
     {
-        _boardGenerator = GameObject.Find("Generator").GetComponent<Generator>();
-        _boardGenerator.CreateNew(Difficulty);
-
         StartCoroutine(TriggerBanner());
-    }
-
-    public void SortIntoCheckGroups(List<Tile> tileList)
-    {
-        if(checkGroups.Length == 0 || checkGroups[0] == null)
-        {
-            checkGroups = GameObject.Find("CheckGroups").GetComponentsInChildren<CheckGroup>();
-        }
-        foreach(CheckGroup group in checkGroups)
-        {
-            group.SortGroup(tileList);
-        }
-    }
-
-    public void PerformCheck()
-    {
-        bool isSolved = true;
-
-        foreach(CheckGroup group in checkGroups)
-        {
-            if (!group.Complete)
-            {
-                isSolved = false;
-                break;
-            }
-        }
-
-        if (isSolved)
-        {
-            GameObject.Find("Victory").GetComponent<UIGroup>().OpenGroup();
-        }
-    }
-
-    public void SetSelection(GameObject selection)
-    {
-        if (CurrentSelection)
-        {
-            _previousSelection = CurrentSelection;
-        }
-        CurrentSelection = selection;
-        _isDirty = true;
-    }
-
-    public void SetValue(int val)
-    {
-        if (CurrentSelection)
-        {
-            CurrentSelection.GetComponent<Tile>().SetValue(val);
-        }
-    }
-
-    private void LateUpdate()
-    {
-        if (CurrentSelection)
-        {
-            if (_isDirty)
-            {
-                if (_previousSelection)
-                {
-                    _previousSelection.GetComponent<Tile>().BackPlate.color = Color.white;
-                }
-                CurrentSelection.GetComponent<Tile>().BackPlate.color = SelectionColor;
-
-                _isDirty = false;
-            }
-        }
     }
 
     public void QuitGame()
@@ -118,22 +19,6 @@ public class GameplayManager : MonoBehaviour
     }
     public void NewGame(string difficulty)
     {
-        switch (difficulty)
-        {
-            case ("Easy"):
-                Difficulty = Generator.GameMode.Easy;
-                break;
-            case ("Medium"):
-                Difficulty = Generator.GameMode.Medium;
-                break;
-            case ("Hard"):
-                Difficulty = Generator.GameMode.Hard;
-                break;
-            case ("Expert"):
-                Difficulty = Generator.GameMode.Expert;
-                break;
-        }
-
         SceneManager.LoadScene("Game");
 
         if (Ads)
--- Assets/Scripts/Tile.cs
+++ Assets/Scripts/Tile.cs
@@ -4,71 +4,7 @@ using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
 
-public class Tile : MonoBehaviour
+public class Tile : Selectable
 {
-    public TMP_Text Label;
-    public Image BackPlate;
-    public Color InactiveColor;
-    public Dictionary<string, int> TileID = new Dictionary<string, int>();
-    public bool IsFilled;
-    public List<CheckGroup> CheckGroups = new List<CheckGroup>();
-
-    private GameplayManager _mgmt;
-    private bool _isSelectable = true;
-
-    private void Start()
-    {
-        _mgmt = GameObject.Find("MGMT").GetComponent<GameplayManager>();
-    }
-
-    public int CurrentNumber { 
-        get { 
-            return _currentNumber; 
-        } 
-        set { 
-            Label.text = value.ToString();
-            _currentNumber = value;
-        } 
-    }
-    private int _currentNumber;
-
-    public void Show(bool isHiding)
-    {
-        Label.enabled = isHiding;
-
-        if (isHiding)
-        {
-            BackPlate.color = InactiveColor;
-            _isSelectable = false;
-            IsFilled = true;
-        }
-    }
-
-    public void Select()
-    {
-        if (_isSelectable)
-        {
-            _mgmt.SetSelection(this.gameObject);
-        }
-    }
-
-    public void SetValue(int val)
-    {
-        CurrentNumber = val;
-
-        if(val != 0)
-        {
-            Label.enabled = true;
-            IsFilled = true;
-        } else
-        {
-            Label.enabled = false;
-            IsFilled = false;
-        }
-
-        foreach(CheckGroup group in CheckGroups)
-        {
-            group.PerformCheck();
-        }
-    }
+    
 }
